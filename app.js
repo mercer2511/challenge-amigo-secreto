ec664908dc464cf43f86594ba26b39a38ec15fe1
mercer2511 <98627279+mercer2511@users.noreply.github.com>
2025-01-28 19:04:44 -0500

    Capturar valor y Validación de entrada

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo')
+    if (nombreAmigo.value === '') {
+        alert('Debe ingresar un nombre')
+        return
+    } else {
+        amigos.push(nombreAmigo.value)
+    }
+    console.log(amigos)    
+}
