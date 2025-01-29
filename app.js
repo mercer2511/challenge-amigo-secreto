00a0a6ff1106b5fc3f11b5067df54d02e2fb981c
mercer2511 <98627279+mercer2511@users.noreply.github.com>
2025-01-28 19:42:20 -0500

    se creo función para mostrar la lista de amigos

--- app.js
+++ app.js
@@ -2,13 +2,24 @@
 let amigos = []
 
 function agregarAmigo() {
-    let nombreAmigo = document.getElementById('amigo')
+    let nombreAmigo = document.getElementById('amigo');
     if (nombreAmigo.value === '') {
-        alert('Debe ingresar un nombre')
-        return
+        alert('Debe ingresar un nombre');
+        return;
     } else {
-        amigos.push(nombreAmigo.value)
-        nombreAmigo.value = ''
-    }
-    console.log(amigos)    
+        amigos.push(nombreAmigo.value);
+        nombreAmigo.value = '';
+        mostrarAmigos();
+        console.log(amigos);
+        return;
+    };
+        
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    };
 }
