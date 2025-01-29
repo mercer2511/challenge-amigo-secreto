598912453f52557b28eced85f997afc1dfd0252e
mercer2511 <98627279+mercer2511@users.noreply.github.com>
2025-01-28 19:58:05 -0500

    Se implemento la función para el sorteo y se añadio comentarios en el código.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Se creo el array amigos para almacenar los nombres de los amigos
 let amigos = []
 
-function agregarAmigo() {
+//Se creo la funcion agregarAmigo para agregar los nombres de los amigos al array amigos
+function agregarAmigo() {    
     let nombreAmigo = document.getElementById('amigo');
     if (nombreAmigo.value === '') {
         alert('Debe ingresar un nombre');
         return;
-    } else {
-        amigos.push(nombreAmigo.value);
+    } else {        
+        amigos.push(nombreAmigo.value);        
         nombreAmigo.value = '';
         mostrarAmigos();
         console.log(amigos);
@@ -16,10 +19,24 @@ function agregarAmigo() {
         
 }
 
-function mostrarAmigos() {
-    let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = '';
+//Se creo la funcion mostrarAmigos para mostrar los nombres de los amigos en la lista
+function mostrarAmigos() {    
+    let lista = document.getElementById('listaAmigos');    
+    lista.innerHTML = '';    
     for (let i = 0; i < amigos.length; i++) {
         lista.innerHTML += `<li>${amigos[i]}</li>`;
     };
-}
+    }
+
+//Se creo la funcion sortearAmigo para sortear un amigo de la lista de amigos
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Deben haber al menos dos amigos para sortear');
+        return;
+    } else {
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+        return;
+        }
+    };
